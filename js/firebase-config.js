73428696d5b5e58419c5af0025ef10dc2b429b4e
NILOZER <117843578+NILOZER@users.noreply.github.com>
2026-06-25 09:41:34 +0500

    Update Firebase configuration with new project details

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,14 +2,14 @@
 // Replace with your own Firebase project config
 // Get this from: Firebase Console > Project Settings > General > Your apps > Web app
 const firebaseConfig = {
-  apiKey: "AIzaSyAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "000000000000",
-  appId: "1:000000000000:web:aaaaaaaaaaaaaaaaaaaaaa"
+  apiKey: "AIzaSyDdjln2SOCzAveMI8UykdxSqwk8OBKrrJU",
+  authDomain: "major-28949.firebaseapp.com",
+  projectId: "major-28949",
+  storageBucket: "major-28949.firebasestorage.app",
+  messagingSenderId: "990191251963",
+  appId: "1:990191251963:web:2a486b4b35adb247a31b5e",
+  measurementId: "G-9K039F2517"
 };
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
